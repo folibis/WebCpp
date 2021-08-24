38aa7b1927efba46f781aa05442d0e26cef4d8cc
Ruslan Muhlinin <ruslan@muhlinin.com>
2021-08-24 12:32:06 +0300

    add MIT license

--- include/IError.h
+++ include/IError.h
@@ -1,9 +1,26 @@
-/**************************************************************************
-/// \brief The IError class
-/// \author ruslan@muhlinin.com
-/// \date July 25, 2021
-/// \details The error handling interface
-**************************************************************************/
+/*
+*
+* Copyright (c) 2021 ruslan@muhlinin.com
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+*/
 
 #ifndef IERROR_H
 #define IERROR_H
--- include/ResponseWebSocket.h
+++ include/ResponseWebSocket.h
@@ -1,9 +1,33 @@
+/*
+*
+* Copyright (c) 2021 ruslan@muhlinin.com
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+*/
+
 #ifndef RESPONSEWEBSOCKET_H
 #define RESPONSEWEBSOCKET_H
 
 #include "common.h"
 #include "ICommunication.h"
-
+#include "common_ws.h"
 
 namespace WebCpp
 {
@@ -11,14 +35,6 @@ namespace WebCpp
 class ResponseWebSocket
 {
 public:
-    enum class MessageType
-    {
-        Undefined = 0,
-        Text = 1,
-        Binary = 2,
-        Ping = 9,
-        Pong = 10,
-    };
 
     ResponseWebSocket(int connID);
     ResponseWebSocket(const ResponseWebSocket& other) = delete;
