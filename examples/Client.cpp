bc8575263992588ae587e0608a8269baaf4602e5
Ruslan Muhinin <ruslan@muhlinin.com>
2021-09-08 14:23:55 +0300

    add http cient example

---
+++ examples/Client.cpp
@@ -0,0 +1,51 @@
+#include <signal.h>
+#include "common.h"
+#include "HttpClient.h"
+#include "Request.h"
+#include <string>
+#include <iostream>
+#include "StringUtil.h"
+#include "FileSystem.h"
+#include "Url.h"
+
+
+static WebCpp::HttpClient *ptr = nullptr;
+
+
+void handle_sigint(int)
+{
+    if(ptr != nullptr)
+    {
+        ptr->Close(false);
+    }
+}
+
+int main()
+{
+    WebCpp::HttpClient httpCient;
+    ptr = &httpCient;
+
+    signal(SIGINT, handle_sigint);
+
+    WebCpp::HttpConfig config;
+    config.SetHttpProtocol("HTTP");
+    config.SetHttpServerPort(8080);
+
+    httpCient.SetResponseCallback([](const WebCpp::Response &response) -> bool
+    {
+        std::cout << "response code: " << response.GetResponseCode() << " " << response.GetResponsePhrase() << std::endl;
+        auto str = StringUtil::ByteArray2String(response.GetBody());
+        std::cout << str << std::endl;
+        return true;
+    });
+
+    httpCient.Run();
+
+    if(httpCient.Init(config))
+    {
+        httpCient.Open(WebCpp::Http::Method::GET, "http://www.google.com"); // httpbin.org/html
+    }
+
+    httpCient.WaitFor();
+
+}
