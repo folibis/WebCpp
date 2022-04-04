0a14c4e027117c7af79cdd830514d7b3fbdd1522
Ruslan Muhlinin <ruslan@muhlinin.com>
2022-04-04 16:22:34 +0300

    update examples with license and short decription

--- examples/WebSocketServer.cpp
+++ examples/WebSocketServer.cpp
@@ -1,10 +1,39 @@
+/*
+*
+* Copyright (c) 2021 ruslan@muhlinin.com
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+*/
+
+/*
+ * WebSocketServer - a simple WebSocket server that just sends
+ * periodically a random number in specified range and delay
+ * Included HTTP server provides a simple test page.
+*/
+
 #include <signal.h>
 #include "common_webcpp.h"
 #include "defines_webcpp.h"
 #include "HttpServer.h"
 #include "WebSocketServer.h"
 #include "Request.h"
-#include "StringUtil.h"
 #include "ResponseWebSocket.h"
 #include "StringUtil.h"
 #include "DebugPrint.h"
